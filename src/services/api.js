6c8ba0d67cf1e812e2ec1da5647d2ff492e11e1a
Amnan <amnanbudikusuma@gmail.com>
2025-12-15 20:48:58 +0700

    fix api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
@@ -11,15 +11,15 @@ api.interceptors.request.use(
   (config) => {
     // 1. Coba ambil dari key 'user_data' (Nama yang umum dipakai)
     // GANTI 'user_data' DENGAN NAMA KEY YANG KAMU LIHAT DI LANGKAH 1 TADI!
-    const storedUser = localStorage.getItem('user_data'); 
-    
+    const storedUser = localStorage.getItem('user_data');
+
     console.log("--- DEBUG AXIOS ---");
     console.log("1. Raw LocalStorage:", storedUser);
 
     if (storedUser) {
       try {
         const parsedUser = JSON.parse(storedUser);
-        
+
         // 2. Ambil token (Pastikan propertinya bernama 'token')
         const token = parsedUser.token;
         console.log("2. Token Extracted:", token ? "Ada (Panjang: " + token.length + ")" : "KOSONG");
@@ -36,7 +36,7 @@ api.interceptors.request.use(
     } else {
       console.warn("LocalStorage kosong! User dianggap belum login ❌");
     }
-    
+
     return config;
   },
   (error) => Promise.reject(error)
